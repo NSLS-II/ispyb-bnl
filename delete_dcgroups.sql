f249a68eedd3a05aed2b0a168a5773c4d40cd6d1
Karl Levik <karl.levik@diamond.ac.uk>
2018-02-23 02:44:22 +0000

    Delete, have moved

--- delete_dcgroups.sql
+++
@@ -1,4 +0,0 @@
--- This will delete all data collection groups and data collections and any other records 
--- where the table has a cascading foreign key pointing to the deleted data collection group records
-
-DELETE FROM DataCollectionGroup WHERE dataCollectionGroupId >= 1054269;
